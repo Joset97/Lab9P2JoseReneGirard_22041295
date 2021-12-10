5982a0747342547edb93b7e95ab45de87465dc68
Joset97 <93399550+Joset97@users.noreply.github.com>
2021-12-10 15:44:48 -0600

    Listo modificar y agregar

--- src/JamesApp/UsuariosAdminJBDC.java
+++ src/JamesApp/UsuariosAdminJBDC.java
@@ -8,42 +8,50 @@ package JamesApp;
 import JDBC.Dba;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
 
 /**
  *
  * @author jrgir
  */
 public class UsuariosAdminJBDC {
-    
+
     Dba db = new Dba("./Usuarios.mdb");
 
     public UsuariosAdminJBDC() {
     }
-    
-    public void Agregar(String Username, String password, String nombre, int edad, String tipo){
-    
-            //agregar
-      
+
+    public void Agregar(String Username, String password, String nombre, int edad, String tipo) {
+
+        //agregar
         db.conectar();
         try {
-       
+
             db.query.execute("INSERT INTO User"
-                    + " (cuenta,nombre)"
-                    + " VALUES ('" + c + "', '" + n + "')");
+                    + " (Usuario,Nombre,Password,Edad,Tipo)"
+                    + " VALUES ('" + Username + "', '" + nombre + "', '" + password + "', '" + edad + "', '" + tipo + "')");
             db.commit();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         db.desconectar();
-        
 
+    }
     
+    public void modificar(String Username, String password, String nombre, int edad, String tipo, JTable tabla ){
+    
+    db.conectar();
+        try {
+           db.query.execute("update User set  Usuario="+Username+",Nombre="+nombre+" Password="+password+", Edad="+edad+") where"+tabla.isBackgroundSet());
+            db.commit();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        db.desconectar();
+
     
     
     }
     
-    
-    
-    
-    
+
 }
